cbc487bdaf6cf84aa1d69adc51686d96ac2d7f34
BeanLeaf <gashumbaaimable@gmail.com>
2026-01-12 21:49:07 -0800

    Revert to Laravel 12 with PHP 8.2 requirement

--- routes/console.php
+++ routes/console.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-// Schedule subscription processing to run daily at 8:00 AM
-Schedule::command('subscriptions:process')
-    ->dailyAt('08:00')
-    ->withoutOverlapping()
-    ->runInBackground()
-    ->onOneServer();
